838a7e311c4ee53bae91fd2fd117352e47f7e652
Turgut Aydemir <turgut.aydemir@compax.at>
2023-12-04 12:08:41 +0100

    HighScore logic with SampleScene adjustments

--- Assets/collision.cs
+++ Assets/collision.cs
@@ -14,11 +14,13 @@ public class collision : MonoBehaviour
     static collision inst;
     private int puddleContact = 0;
     private int score = 0;
+    public static int highScore; //We pass the PlayerPrefs(HighScore) to this variable and then use it to show actual highScore
     public float initialSpeed = 6f;
     public float speedIncrement = 1.1f;
     public float maxSpeed = 36f;
 
     [SerializeField] TMP_Text scoreText;
+    [SerializeField] TMP_Text highScoreText;
     [SerializeField] lane_movement lane_movement;
 
 
@@ -30,24 +32,40 @@ public class collision : MonoBehaviour
         animator = doenerMann.GetComponent<Animator>();
         audioSource = GameObject.Find("AudioItems").GetComponent<AudioSource>();
         audioSource2 = GameObject.Find("AudioGameover").GetComponent<AudioSource>();
-
         //Call periodically to increase score
         InvokeRepeating("IncreasePoints", 0f, 2f);
+        //We check HighScore at the Start to show on the main playScreen
+        HighScoreSet();
     }
 
 
     public void ScoreIncrement()
     {
         this.score += 5;
-        scoreText.text = "SCORE: " + this.score;
-
+        scoreText.text = "Score: " + this.score;
+    
     }
+
+    public void HighScoreSet()
+    {
+        //We pass the PlayerPrefs(HighScore) to int highScore
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        //We change the highScoreText to the PlayerPrefs(HighScore) with a String value of it
+        highScoreText.text = "HighScore: " + highScore.ToString();
+        //if the current score is greater than the HighScore in PlayerPrefs
+        if (score > highScore)
+        {
+            //We change the HighScore in PlayerPrefs with the new score
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+    }
+
     public static collision Instance { get; private set; }
 
     private void Awake()
     {
         scoreText = GameObject.Find("Score").GetComponent<TMP_Text>();
-
+        highScoreText = GameObject.Find("HighScore").GetComponent<TMP_Text>();
         if (Instance == null)
         {
             Instance = this;
@@ -57,14 +75,19 @@ public class collision : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        //each Frame update checks if HighScore is exceeded to show it directly during game
+        HighScoreSet();
+    }
+
     private void OnTriggerEnter(Collider other){
        
         if (other.gameObject.CompareTag("NewCoin"))
         {
             audioSource.Play();
             this.score += 5;
-            scoreText.text = "SCORE: " + this.score;
-
+            scoreText.text = "Score: " + this.score;
             Destroy(other.gameObject);
         }
 
@@ -72,8 +95,7 @@ public class collision : MonoBehaviour
         {
             audioSource.Play();
             this.score += 50;
-            scoreText.text = "SCORE: " + this.score;
-
+            scoreText.text = "Score: " + this.score;
             Destroy(other.gameObject);
         }
 
@@ -86,7 +108,6 @@ public class collision : MonoBehaviour
             playGameOverAnimation();
 
         }
-
 
         if (other.gameObject.CompareTag("Puddle"))
         {
@@ -108,8 +129,7 @@ public class collision : MonoBehaviour
         if (move.isMovementAllowed)
         {
             score++;
-            scoreText.text = "SCORE: " + score;
-
+            scoreText.text = "Score: " + score;
             if (score % 5 == 0)
             {
                 move.speedIncreasementFactor += 2.0f;
